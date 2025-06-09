0cea26a36cd3a7f7aa9ebfee8424704bb7f9f8f5
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-06-09 22:27:05 +0300

    generate policy

---
+++ Bot.BLL/Policy/PolicyGenerator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Bot.BLL.Policy;
+
+public class PolicyGenerator
+{
+    public string GeneratePolicy(string clientName, string vin)
+    {
+        return
+            $"📝 Insurance policy\nName: {clientName}\nVIN: {vin}\nSum: 100 USD\nDate: {DateTime.UtcNow:dd.MM.yyyy}";
+    }
+}
